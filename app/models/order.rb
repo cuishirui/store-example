7d036bedf42eff2454adf15264b203c21dc233e7
cuishirui <344494024@qq.com>
2017-03-27 11:47:39 +0800

    add orders show function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
 
-  belongs_to :user 
+  belongs_to :user
+
+  has_many :product_lists
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326095734) do
+ActiveRecord::Schema.define(version: 20170327030142) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20170326095734) do
     t.string   "billing_address"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
