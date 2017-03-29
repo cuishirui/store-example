d22b9913892d9e14eb757214b056bd55b76a11c9
cuishirui <344494024@qq.com>
2017-03-29 16:52:00 +0800

    implement admin/orders show function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328031510) do
+ActiveRecord::Schema.define(version: 20170329073505) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -30,11 +30,13 @@ ActiveRecord::Schema.define(version: 20170328031510) do
     t.integer  "user_id"
     t.string   "billing_name"
     t.string   "billing_address"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.string   "token"
     t.boolean  "is_paid",         default: false
     t.string   "payment_method"
+    t.string   "aasm_state",      default: "order_placed"
+    t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
   create_table "product_lists", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "18551038995@163.com", password: "cuishirui", password_confirmation: "cuishirui", is_admin: "true")
+puts "admin acount created"
+create_product = for i in 1..10 do
+  Product.create(title: "things#{i}", description: "sssxx#{i}", quantity: rand(22..33)*88, price: rand(898..8098)*100)
+end
+puts "10 product created"
